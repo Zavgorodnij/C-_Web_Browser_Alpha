22e2cbfb2c3d3b43ce13036196d26d5b2dc41f6a
Evgenij Zavgorodnij <razorval99@gmail.com>
2019-10-22 14:57:03 +0300

    Update and rename WpfApp1/App.xaml.cs to  WpfApp1/App.xaml.cs

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,3 +1,7 @@
+https://docs.google.com/spreadsheets/d/1IlBTFC9p54udwn0PfCfgObjKCf_gQp63pMgwauvziMI/edit?usp=sharing
+[Ссылка]
+Таблица посещаемости 2019/2020
+https://docs.google.com/spreadsheets/d/1IlBTFC9p54udwn0PfCfgObjKCf_gQp63pMgwauvziMI/edit?usp=sharing
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
